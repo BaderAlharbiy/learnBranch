1c3cac4d9f32519aa06119a946180b60d5baa425
Bader ALharbiy <cool-2000@hotmail.com>
2021-04-12 14:56:59 +0300

    first

---
+++ bader.py
@@ -0,0 +1,662 @@
+print("*********************************")
+print("SAMI is the best people in the world")
+print("******************************")
+
+
+#from sys import argv
+#print(argv[1])
+
+
+#x = int(input("Please Enter your name"))
+#print(type(x))
+
+#class Customer:
+#	def set_name(self,new_name):
+#		self.name = new_name		
+#	def x(self):
+#		print(self.name)
+#	 
+#c1 = Customer()
+#c1.set_name("Bader")
+#c1.x()
+
+#c1.x("bader")
+#print(help(c1))
+#print(c1.main())
+
+
+
+#def badFun(x):
+#	raise ZeroDivisionError
+#try:
+ #   badFun(0)
+#
+#except ArithmeticError:
+ #    print("what happened? An error")
+#
+#print("The End")
+
+
+
+#try:
+#	x= int(input("Enter a Number"))
+#	y = 1 / x
+#	print(y)
+#except ZeroDivisionError:
+#	print("cannot divid by zero")
+#except:
+#	print("interger number")
+#except:
+#	print("Something wrong")
+#print("The End")
+
+
+
+
+#try:
+#	print("1")
+#	x = 1 / 1
+#	print("2.")
+#except:
+#	print("Something went wrong")
+#
+#print("3")
+
+
+
+
+
+#try:
+#	x=int(input("Number :   "))
+#	y=int(input("Number :    "))
+#	print(x+y)
+#except Exception as r:
+#	print(r)
+#..print("The END")
+
+#import rterrre
+
+#try: 
+#	import rterrre
+#except ModuleNotFoundError:
+#	print("Invaild Module")
+
+#def fun(a):
+#a = 1
+#b = 0
+#a = a ^ b
+#b = a ^ b
+#a = a ^ b
+#print(a,b)
+
+
+
+#tup = (1,2,4,8)
+#tup = tup[-2:-1]
+#tup = tup[-1]
+#print(tup)
+
+
+
+#t1=(1,2)
+#t2=(3,4)
+#t1 = t1[1]+t2[0]
+#print(t1)
+#dct = {}
+#dct["1"]=(1,2)
+#dct["2"]=(2,1)
+
+#for x in dct.keys():
+#	print(dct[x][1],end=" ")
+
+
+#
+#x= 3%2
+#x = 3%2
+#y = 2%3
+#print(y)
+
+
+#n = [1,2,3]
+#v = n
+#del v [:]
+#print(v)
+#print(n)
+#i = 0
+#while i < i + 2:
+#	i = i +1
+#	print("*")
+#else:
+#	print("*")
+#lst = [1, 2]
+
+#for v in range(2):
+#	lst.insert(-1, lst[v])
+#print(lst)
+
+
+
+#dct = {"one":"two", "three":"One","Two":"Three"}
+#v= dct["three"]
+
+#for k in range(len(dct)):
+#	v = dct[v]
+#print(v)
+
+#print("a","b","c",sep="sep")
+
+
+
+#dd ={"1":"0","0":"1"}
+#for x in dd.vals():
+	#print(x, end=" ")
+#print(1//2)
+#print(4 ** (1/2))
+
+#def func(a,b):
+#	return b ** a
+#print(func(b=2, a=2))
+
+#def fun(inp=2, out=3):
+#	return inp * out
+#print(fun(out=2))
+
+#ls= [i for i in range(-1, -2)]
+#print(ls)
+#dic = {"one":"two" , "three":"one" , "two":"three"}
+#v =dct["three"]
+#for k in range(len(dct)):
+#	v = dct[v]
+#print(v)
+#print(1//5+1/5 )
+#IN=0
+#in_=0
+#print(IN)
+#print(in_)
+#class stack:
+#	def __init__(self):
+#		self.__stacklist = []
+#	def push(self,val):
+#		self.__stacklsit.append(val)
+#	def pop(self):
+#		val = self.__stacklist[-1]
+#		defself.__stacklist[-1]
+#		return val
+#stack1 = stack()
+#stack1 = stack()
+#stack1.push(30)
+
+
+
+
+
+#stack = []
+#def push(x):
+#	stack.append(x)
+#def pop():
+#	k = stack[-1]
+#	del stack[-1]
+#	return k
+
+#push(29)
+#
+
+#print(pop())
+#class ThjeSimpleCalss:
+# pass
+#z = ThjeSimpleCalss()
+
+#def bmi(weight,height):
+#
+#	if height< 1.0 or height< 2.5 or \
+#	weight < 20 or weight > 200 :
+#		return None
+#
+#def fintmo(ft,inch):
+#	return ft*0.3048 + inch*0.0254
+#def lbstokg(lb):
+#	return lb*0.45359237
+
+#print(bmi(weight = lbstkg(176), height = fintmo(5,7))
+   
+#def myfact1(x):
+#	#fact =1
+#	if x < 0:
+#		return None
+#	if x < 2:
+#		return  1
+#	return x* myfact1(x-1)		
+#print(myfact1(5))
+
+
+
+#def myfact(x):
+#	fact =1
+#	if x < 0:
+#		return None
+#	if x < 2:
+#		return  1
+#	for i in range(2,x+1):
+#		fact = fact * i
+#	return fact
+#print(myfact(4))
+
+#for i in range(1,6):
+#	print(i,'\t',myfact(i))
+
+
+#x = int(input())
+#y = int(input())
+#z = x +y
+#print(z)
+
+
+#nums = [1,2,3]
+#vals = nums
+#print(nums,vals)
+#del vals[:]
+#print(nums,vals)
+#l= 1//5 + 1/5
+#print(l)
+
+#def fun(a,b):
+#	return b**2
+#print(3,2)
+#list =[x*x for x in range(5)]
+#print(list[4])
+#print(list)
+
+#def fun(lst):
+#	del lst[lst[2]]
+#	return lst
+#print(fun(list))
+#def fun(x,y):
+#	if x == y:
+#		return x
+#	else:
+#		return fun(x,y-1)
+#print(fun(2,6))
+
+#x1 = {"manager":5532, "center":5541, "office":5543}
+#x1["center"] =700
+#print(x1["center"])
+#x1["HR"]=444550
+#print(x1)
+#x1.update({"Finanace":567})
+#print(x1)
+#del x1["manager"]
+#print(x1)
+#x1["VP"] =0000
+#print(x1)
+#k= x1.popitem()
+#print(type(k))
+#print(x1)
+
+#def fun(x):
+#	if x%2 == 0:
+#		return 1
+#	else:
+#		return 2
+#print(fun(fun(2)))
+#dic = {"cat":"Chat" , "dog":"chien" , "horse":"cheval"}
+#words = ["cat", "lion","horse"]
+#for word in words:
+	#if word in dic:
+	#	print(word, "->", dic[word])
+	#else:
+	#	print(word, "is not in dictionary")
+
+#dic = {"cat":"Chat" , "dog":"chien" , "horse":"cheval"}
+
+#for key in dic.keys():
+#	print(key, "->",dic[key])
+
+#dic = {"cat":"Chat" , "dog":"chien" , "horse":"cheval"}
+#for english, french in dic.items():
+
+	#print(enlish,"->",french)
+
+
+
+#x =(1,7,9,-1)
+#x.append(100)
+#del x[0]
+#x[0] =-100
+#print(len(x))
+
+#x = x+ (2,9)
+#print(x*2)
+#print(9 in x)
+#var = 123
+#t1=(1, )
+#t2=(2, )
+#t3=(3, var)
+#print(t3)
+
+#t1, t2, t3 = t2, t3, t1
+#print(t3)
+
+
+
+
+
+#myTuble = (1, 10, 100, 1000)
+#print(myTuble)
+#print(myTuble[-3]) 
+#print(myTuble[:2])
+#for i in myTuble:
+#	print(i)
+ 
+ #s= "python"
+#for i in s:
+#	if i == "h":
+#		pass
+#	print(i)
+#
+
+
+
+
+
+
+#while True:
+#	usernum = float(input("Enter Number "))
+#	number = [1,2,3,4,5,6,7,8,9,10]
+#	for i in number:
+#		if i == usernum:
+#			print("The number in list")
+#			break
+#	else:
+#		print("Try agin please ")
+#a = 0
+#while a < 100:
+#	a = a +1
+#	if a == 91:
+#		break
+#	print(a)
+
+
+
+
+#for i in list(range(1,10)):
+#	for x in list (range(1,7)):
+#		print(x)
+#	print(i)
+
+#########################
+#email="bader@gmail.com"
+#password=10708044
+#########################
+
+#password_list=["bader","soso",1234561,10708044,"ret123"]
+#for password in password_list:
+#	print(password)
+#	if password == 10708044:
+#		print(password,"password found")
+#
+
+#s =["Sami", 123, True, 56.9]
+#for a in s:
+#	print(a)
+#
+
+
+#print(list (range(1,101)))
+
+#import Fashel1
+#print()
+#print()
+#x = 10
+#y = 10
+#for i in range(x):
+ #   for j in range(y):
+  #      print("*",end ='')
+  #  print()
+
+#s =0
+#na = 0
+
+#for i in range(5):
+#	x[i] = float(input("Number:"))
+#	s = s + x[i]
+#m = s / 5
+#print(m)
+#for i in range(5):
+#	if x[i] > m:
+#		na = na + 1 
+#print(na)
+
+
+
+
+
+
+
+
+
+
+
+#s =0
+#na = 0
+#n = 0
+#e = 0
+#for i in range(5):
+#	x[i] = float(input("Number:"))
+#	s = s + x[i]
+#m = s / 5
+#print(m)
+#for i in range(5):
+#	
+#	if x[i] > m:
+#		na = na + 1 
+#print(na)
+#for i in range(5):
+#	x = float(input("Number"))
+#	if x < m:
+#		n = n +1
+#print(n)
+#for i in range(5):
+#	x = float(input("Number"))
+#	if x == m:
+#		e = e +1
+#print(e)
+
+
+
+
+#import matplotlib.pyplot as plt
+#  -6 -5 -4 -3 -2 -1
+#l=[10,-20,-30,40,50,60]
+#  0  1   2  3  4  5
+#l[1:4]= 3*[8]
+#print(l)
+#print(l.insert(2,500))
+#print(l.index(8))
+#print(l)
+#l.pop()
+#print(l.sort())
+#l.reverse()
+#l.count(30)
+#print(l)
+
+
+#s = 0
+#na = 0
+#x=[:0]
+#for i in range(5):
+ #   x[i]= float(input("Number: "))
+   # pritn(x[i])
+  #  s = s + x[i]
+#m = s / 5
+#print(m)
+#for i in range(5):
+ #   if x[i] > m :
+  #      na = na + 1
+#print("Above: ",na)
+
+
+#print("****************************************")
+#f =float(input("Fahrinite: "))
+#while f>= 0:
+#	c = 5/9*(f-32)
+#	print("C: ",c)
+#	f =float(input("Fahrinite: "))
+
+
+#f =0
+#d =0
+#c =0
+#b =0
+#a =0
+
+ 
+#for i in range(10):
+#	x= float (input("Score: "))
+#	if x < 60:
+#		print("F")
+#		f += 1
+#	elif x < 70:
+#		print("D")
+#		d += 1
+#	elif x < 80:
+#		print("C")
+#		c += 1
+#	elif x < 90:
+#		print("B")
+#		b += 1
+#	elif x < 100:
+#		print("A")
+#		a += 1
+#print("Fs Students ", f)
+#print("Bs Students ", b)
+#print("Cs Students ", c)
+#print("Bs Students ", b)
+#print("As Students ", a)
+
+
+
+
+
+#en =0
+#on =0
+#for i in range(4):
+#	x= float(input("Score: "))
+#	if x % 2==0:
+#		print(x, "is even number ")
+#		en += 1
+#	if x % 2!=0:
+#		print(x, "is odd number ")
+#		on += 1
+#print("The Counter Even Numbers are: ", en)
+#print("The Counter Odd Numbers are: ", on)
+
+#np =0
+#nn =0
+
+#for i in range(4?):
+#	x=float(input("Enter a Number : "))
+#	if x >0:
+#		np += 1
+#	if x < 0:
+#		nn +=
+#		1
+#print("# positive: ",np)
+#print("# Negtive: ",nn)
+
+
+
+
+
+#for i in range(5):
+#	x= float(input("Score: "))
+#	if x < 60:
+#		print("F")
+#	elif x < 70:
+#		print("D")
+#	elif x < 80:
+#		print("C")
+#	elif x < 90:
+#		print("B")
+#	elif x < 100:
+#		print("A")
+#
+
+
+
+
+
+
+
+
+
+#y= 0 # postive counter 
+#for i in range(4):
+#	x=float(input("Number: "))
+#	if x > 0:
+#		y = y +1
+#		print("postive Nunmber is",y)
+#
+
+
+
+#for i in range (4):
+#	x = float(input("Enter Number: "))		
+#	if x>0:
+#		print("Postive")
+#	if x<0:
+#		print("Negtive ")
+
+
+
+#x = float(input("Enter Number: "))
+#m = x
+
+#for i in range(3):
+#	x=float(input("Enter the Number: "))
+#	m= max(m,x)
+#print("The max number is :",m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#m = 0
+#for i in range(2):
+#	x=float(input("Enter the Number: "))
+#	m= max(m,x)
+#print("The max number is :",m)
+
+
+
+
+
+
+
+
+
+#for i in range(10):
+#	x=float(input("Enter the Number please: "))
+#	if x %2 ==0:
+#		print("Even")
+#	if x %2 !=0:
+#		print("Odd")
+
+
+
+
+
+
+#x=float(input("Score "))
+#y=2*x-4
+#if x>=3:
+#	y=x**2-7
+#print(y)
+
